1cece6ad6cdaf37c42f38e9eaab179ace3f4ca2e
dunca <flv@mail.com>
2018-04-10 21:02:55 +0300

    AcquisitionActivity: basic implementation

---
+++ app/src/main/java/dunca/github/io/logpurchasemanager/activities/AcquisitionActivity.java
@@ -0,0 +1,15 @@
+package dunca.github.io.logpurchasemanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dunca.github.io.logpurchasemanager.R;
+
+public class AcquisitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acquisition_header);
+    }
+}
