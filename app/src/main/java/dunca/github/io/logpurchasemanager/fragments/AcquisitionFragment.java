4f5f20866f03d3b5f3af6484e8e1605da2141ff2
dunca <flv@mail.com>
2018-04-11 14:39:27 +0300

    AcquisitionFragment: implemented saveAcquisitionSerialNumber(), updateUiWithDefaults(), createDefaultSpinnerAdapter()

--- app/src/main/java/dunca/github/io/logpurchasemanager/fragments/AcquisitionFragment.java
+++ app/src/main/java/dunca/github/io/logpurchasemanager/fragments/AcquisitionFragment.java
@@ -1,17 +1,24 @@
 package dunca.github.io.logpurchasemanager.fragments;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import dunca.github.io.logpurchasemanager.R;
 import dunca.github.io.logpurchasemanager.constants.MethodParameterConstants;
@@ -22,9 +29,13 @@ import dunca.github.io.logpurchasemanager.data.model.AcquisitionStatus;
 import dunca.github.io.logpurchasemanager.data.model.Supplier;
 import dunca.github.io.logpurchasemanager.data.model.WoodCertification;
 import dunca.github.io.logpurchasemanager.data.model.WoodRegion;
+import dunca.github.io.logpurchasemanager.data.model.constants.CommonFieldNames;
+import dunca.github.io.logpurchasemanager.data.model.interfaces.Model;
 
 public class AcquisitionFragment extends Fragment {
     public static final String ACQUISITION_ID_PARAM = "acquisition_id_param";
+
+    private static final String LAST_ACQUISITION_ID_PROP = "last_acquisition_id_prop";
 
     private Acquisition mModifiedAcquisition;
 
@@ -45,18 +56,40 @@ public class AcquisitionFragment extends Fragment {
     }
 
     private void initViews() {
+        DatabaseHelper dbHelper = DatabaseHelper.getLatestInstance();
+
         mEtSerialNumber = mFragment.findViewById(R.id.etSerialNumber);
+        mEtSerialNumber.setText(String.valueOf(getLastAcquisitionSerialNumber() + 1));
 
         mSpinnerAcquirer = mFragment.findViewById(R.id.spinnerAcquirer);
+        List<Acquirer> acquirerList = dbHelper.getAcquirerDao().queryForAll();
+        ArrayAdapter acquirerAdapter = createDefaultSpinnerAdapter(acquirerList);
+        mSpinnerAcquirer.setAdapter(acquirerAdapter);
+
 
         mTvDate = mFragment.findViewById(R.id.tvDate);
         mTvSupplierName = mFragment.findViewById(R.id.tvSupplierName);
 
         mSpinnerWoodRegion = mFragment.findViewById(R.id.spinnerWoodRegionSymbol);
+        List<WoodRegion> woodRegionList = dbHelper.getWoodRegionDao().queryForAll();
+        ArrayAdapter woodRegionAdapter = createDefaultSpinnerAdapter(woodRegionList);
+        mSpinnerWoodRegion.setAdapter(woodRegionAdapter);
 
         mEtRegionZone = mFragment.findViewById(R.id.etRegionZone);
 
         mSpinnerWoodCertification = mFragment.findViewById(R.id.spinnerWoodCertification);
+        List<WoodCertification> woodCertificationList;
+
+        try {
+            woodCertificationList = dbHelper.getWoodCertificationDao()
+                    .queryBuilder().orderBy(CommonFieldNames.LIST_PRIORITY, true)
+                    .query();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        ArrayAdapter woodCertificationAdapter = createDefaultSpinnerAdapter(woodCertificationList);
+        mSpinnerWoodCertification.setAdapter(woodCertificationAdapter);
 
         mEtObservations = mFragment.findViewById(R.id.edObservations);
 
@@ -100,7 +133,12 @@ public class AcquisitionFragment extends Fragment {
         Bundle args = getArguments();
         int acquisitionId = args.getInt(ACQUISITION_ID_PARAM, 0);
 
-        setModifiedAcquisition(acquisitionId);
+        if (acquisitionId != 0) {
+            // the user is trying to update an existing object
+            setModifiedAcquisition(acquisitionId);
+        } else {
+            updateUiWithDefaults();
+        }
 
         return mFragment;
     }
@@ -125,6 +163,20 @@ public class AcquisitionFragment extends Fragment {
         // TODO update ui with data from mModifiedAcquisition
 
 
+    }
+
+    /**
+     * Updates the UI inputs with default values (current date, a valid acquisition serial number,
+     * etc.)
+     */
+    private void updateUiWithDefaults() {
+        int currentAcquisitionSerialNumber = getLastAcquisitionSerialNumber() + 1;
+        mEtSerialNumber.setText(String.valueOf(currentAcquisitionSerialNumber));
+
+        DateFormat isoDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date currentDate = new Date();
+
+        mTvDate.setText(isoDateFormat.format(currentDate));
     }
 
     private void setupOnClickActions() {
@@ -179,5 +231,29 @@ public class AcquisitionFragment extends Fragment {
 
     private WoodCertification getSelectedWoodCertification() {
         return null;
+    }
+
+    private void saveAcquisitionSerialNumber(Acquisition currentAcquisition) {
+        SharedPreferences.Editor editor = getSharedPreferences().edit();
+
+        editor.putInt(LAST_ACQUISITION_ID_PROP, Integer.valueOf(
+                currentAcquisition.getSerialNumber()));
+    }
+
+    private int getLastAcquisitionSerialNumber() {
+        return getSharedPreferences().getInt(LAST_ACQUISITION_ID_PROP, 0);
+    }
+
+    private <T extends Model> ArrayAdapter<T> createDefaultSpinnerAdapter(List<T> modelInstanceList) {
+        ArrayAdapter<T> arrayAdapter = new ArrayAdapter<T>(getContext(),
+                android.R.layout.simple_spinner_item, modelInstanceList);
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        return arrayAdapter;
+    }
+
+    private SharedPreferences getSharedPreferences() {
+        return getActivity().getPreferences(Context.MODE_PRIVATE);
     }
 }
