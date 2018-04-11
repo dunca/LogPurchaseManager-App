c9ec2c8e89f6f8feba05b7198d4a975e340f32d5
dunca <flv@mail.com>
2018-04-11 09:41:01 +0300

    StaticDataGenerator: implemented more object creation methods

--- app/src/main/java/dunca/github/io/logpurchasemanager/data/StaticDataGenerator.java
+++ app/src/main/java/dunca/github/io/logpurchasemanager/data/StaticDataGenerator.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 import dunca.github.io.logpurchasemanager.data.dao.DatabaseHelper;
 import dunca.github.io.logpurchasemanager.data.model.Acquirer;
+import dunca.github.io.logpurchasemanager.data.model.AcquisitionStatus;
+import dunca.github.io.logpurchasemanager.data.model.LogQualityClass;
+import dunca.github.io.logpurchasemanager.data.model.Supplier;
 import dunca.github.io.logpurchasemanager.data.model.TreeSpecies;
+import dunca.github.io.logpurchasemanager.data.model.WoodCertification;
 import dunca.github.io.logpurchasemanager.data.model.WoodRegion;
 
 public final class StaticDataGenerator {
@@ -33,5 +37,34 @@ public final class StaticDataGenerator {
         TreeSpecies treeSpecies2 = new TreeSpecies("sp", "Spruce");
 
         mDatabaseHelper.getWoodSpeciesDao().create(Arrays.asList(treeSpecies1, treeSpecies2));
+    }
+
+    public void createWoodCertifications() {
+        WoodCertification wc1 = new WoodCertification("None");
+        WoodCertification wc2 = new WoodCertification("PEFC");
+
+        mDatabaseHelper.getWoodCertificationDao().create(Arrays.asList(wc1, wc2));
+    }
+
+    public void createLogQualityClasses() {
+        LogQualityClass lc1 = new LogQualityClass("HQ", "High Quality");
+        LogQualityClass lc2 = new LogQualityClass("LQ", "Low Quality");
+
+        mDatabaseHelper.getLogQualityClassDao().create(Arrays.asList(lc1, lc2));
+    }
+
+    public void createAcquisitionStatuses() {
+        AcquisitionStatus as1 = new AcquisitionStatus("Accepted");
+
+        mDatabaseHelper.getAcquisitionStatusDao().create(as1);
+    }
+
+    public void createSuppliers() {
+        Supplier supplier1 = new Supplier("SuperWood", "Wood's street", "Germany", "Berlin",
+                "1234");
+        Supplier supplier2 = new Supplier("UltraWood", "Spruce's street", "Austria", "Vienna",
+                "4567");
+
+        mDatabaseHelper.getSupplierDao().create(Arrays.asList(supplier1, supplier2));
     }
 }
