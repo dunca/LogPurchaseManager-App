e50744817be020d56a756a1a854e206121585db2
dunca <flv@mail.com>
2018-04-11 21:50:02 +0300

    AcquisitionListActivity: implemented RecyclerView adapter and view holder

--- app/src/main/java/dunca/github/io/logpurchasemanager/activities/AcquisitionListActivity.java
+++ app/src/main/java/dunca/github/io/logpurchasemanager/activities/AcquisitionListActivity.java
@@ -1,32 +1,120 @@
 package dunca.github.io.logpurchasemanager.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import dunca.github.io.logpurchasemanager.R;
+import dunca.github.io.logpurchasemanager.data.dao.DatabaseHelper;
+import dunca.github.io.logpurchasemanager.data.model.Acquisition;
 
 public class AcquisitionListActivity extends AppCompatActivity {
+    private FloatingActionButton mFab;
+    private List<Acquisition> mAcquisitionList;
+    private RecyclerView mRvAcquisitions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acquisition_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initViews();
+        setupOnClickActions();
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private void initViews() {
+        mFab = findViewById(R.id.fab);
+        mRvAcquisitions = findViewById(R.id.rvAcquisitionList);
+    }
+
+    private void setupOnClickActions() {
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startMainActivity();
+            }
+        });
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainTabbedActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        mAcquisitionList = DatabaseHelper.getLatestInstance().getAcquisitionDao().queryForAll();
+
+        setupAcquisitionRecyclerView();
+
+        super.onResume();
+    }
+
+    private void setupAcquisitionRecyclerView() {
+        mRvAcquisitions.setLayoutManager(new LinearLayoutManager(this));
+        mRvAcquisitions.setAdapter(new AcquisitionListRecyclerViewAdapter());
+    }
+
+    class AcquisitionListRecyclerViewAdapter extends RecyclerView.Adapter<AcquisitionItemViewHolder> {
+        private final SimpleDateFormat isoDateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        @NonNull
+        @Override
+        public AcquisitionItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(
+                    R.layout.activity_acquisition_list_acquisition_list_item, parent, false);
+
+            return new AcquisitionItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AcquisitionItemViewHolder holder, int position) {
+            Acquisition acquisition = mAcquisitionList.get(position);
+
+            holder.mTvSerialNumber.setText(acquisition.getSerialNumber());
+            holder.mTvAcquirer.setText(acquisition.getAcquirer().getUsername());
+            holder.mTvSupplier.setText(acquisition.getSupplier().getName());
+
+            Date acquisitionDate = acquisition.getReceptionDate();
+            holder.mTvAcquisitionDate.setText(isoDateFormatter.format(acquisitionDate));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mAcquisitionList.size();
+        }
+    }
+
+    class AcquisitionItemViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTvSerialNumber;
+        private TextView mTvAcquirer;
+        private TextView mTvSupplier;
+        private TextView mTvAcquisitionDate;
+
+        public AcquisitionItemViewHolder(View itemView) {
+            super(itemView);
+
+            mTvSerialNumber = itemView.findViewById(R.id.tvSerialNumber);
+            mTvAcquirer = itemView.findViewById(R.id.tvAcquirer);
+            mTvSupplier = itemView.findViewById(R.id.tvSupplierName);
+            mTvAcquisitionDate = itemView.findViewById(R.id.tvAcquisitionDate);
+        }
+    }
 }
