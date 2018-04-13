41ba523876d0b013e8b2397491fcc6b7575ebc93
dunca <flv@mail.com>
2018-04-13 11:36:14 +0300

    AcquisitionItemFragment: implemented initViews()

--- app/src/main/java/dunca/github/io/logpurchasemanager/fragments/AcquisitionItemFragment.java
+++ app/src/main/java/dunca/github/io/logpurchasemanager/fragments/AcquisitionItemFragment.java
@@ -6,13 +6,63 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.sql.SQLException;
+import java.util.List;
 
 import dunca.github.io.logpurchasemanager.R;
 import dunca.github.io.logpurchasemanager.constants.MethodParameterConstants;
+import dunca.github.io.logpurchasemanager.data.dao.DatabaseHelper;
+import dunca.github.io.logpurchasemanager.data.model.LogQualityClass;
+import dunca.github.io.logpurchasemanager.data.model.TreeSpecies;
+import dunca.github.io.logpurchasemanager.data.model.constants.CommonFieldNames;
+import dunca.github.io.logpurchasemanager.data.model.interfaces.Model;
+import dunca.github.io.logpurchasemanager.fragments.util.FragmentUtil;
 
 public class AcquisitionItemFragment extends Fragment {
-    public AcquisitionItemFragment() {
 
+    private View mFragmentView;
+
+    private Spinner mSpinnerSpecies;
+    private Spinner mSpinnerQualityClass;
+    private EditText mEtBarCode;
+    private CheckBox mCbSpecialPrice;
+    private EditText mEtVolumetricPrice;
+    private EditText mEtGrossLength;
+    private EditText mEtGrossDiameter;
+    private EditText mEtNetLength;
+    private EditText mEtNetDiameter;
+    private TextView mTvGrossVolume;
+    private TextView mTvNetVolume;
+    private EditText mEtObservations;
+    private Button mBtnSave;
+
+    private final DatabaseHelper mDbHelper;
+    private List<TreeSpecies> mSpeciesList;
+    private List<LogQualityClass> mLogQualityClassList;
+
+    public AcquisitionItemFragment() {
+        mDbHelper = DatabaseHelper.getLatestInstance();
+
+        initDbLists();
+    }
+
+    private void initDbLists() {
+        try {
+            mSpeciesList = mDbHelper.getTreeSpeciesDao().queryBuilder()
+                    .orderBy(CommonFieldNames.LIST_PRIORITY, true)
+                    .query();
+
+            mLogQualityClassList = mDbHelper.getLogQualityClassDao().queryForAll();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static AcquisitionItemFragment newInstance(int acquisitionId) {
@@ -38,6 +88,11 @@ public class AcquisitionItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        mFragmentView = inflater.inflate(R.layout.fragment_acquisition_item, container, false);
+
+        initViews();
+        setupOnClickActions();
+
         Bundle args = getArguments();
         int acquisitionId = args.getInt(MethodParameterConstants.ACQUISITION_ID_PARAM, 0);
 
@@ -48,8 +103,50 @@ public class AcquisitionItemFragment extends Fragment {
 
         }
 
-        // inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_acquisition_item, container, false);
+        return mFragmentView;
     }
 
+    private void initViews() {
+        mSpinnerSpecies = findViewById(R.id.spinnerSpecies);
+        ArrayAdapter speciesAdapter = createDefaultSpinnerAdapter(mSpeciesList);
+        mSpinnerSpecies.setAdapter(speciesAdapter);
+
+        mSpinnerQualityClass = findViewById(R.id.spinnerQualityClass);
+        ArrayAdapter qualityClassAdapter = createDefaultSpinnerAdapter(mLogQualityClassList);
+        mSpinnerQualityClass.setAdapter(qualityClassAdapter);
+
+        mEtBarCode = findViewById(R.id.etBarCode);
+
+        mCbSpecialPrice = findViewById(R.id.cbSpecialPrice);
+        mEtVolumetricPrice = findViewById(R.id.etPrice);
+
+        mEtGrossLength = findViewById(R.id.etGrossLength);
+        mEtGrossDiameter = findViewById(R.id.etGrossDiameter);
+
+        mEtNetLength = findViewById(R.id.etNetLength);
+        mEtNetDiameter = findViewById(R.id.etNetDiameter);
+
+        mTvGrossVolume = findViewById(R.id.tvGrossVolume);
+        mTvNetVolume = findViewById(R.id.tvNetVolume);
+
+        mEtObservations = findViewById(R.id.etObservations);
+
+        mBtnSave = findViewById(R.id.btnSave);
+    }
+
+    private void setupOnClickActions() {
+        mBtnSave.setOnClickListener(v -> persistAcquisitionItemChanges());
+    }
+
+    private void persistAcquisitionItemChanges() {
+        // TODO
+    }
+
+    private <T extends View> T findViewById(int id) {
+        return mFragmentView.findViewById(id);
+    }
+
+    private <T extends Model> ArrayAdapter<T> createDefaultSpinnerAdapter(List<T> modelInstanceList) {
+        return FragmentUtil.createDefaultSpinnerAdapter(getContext(), modelInstanceList);
+    }
 }
