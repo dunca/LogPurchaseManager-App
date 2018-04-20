cb5f159854b6dd5e259733a166da1b2fffd0a35d
dunca <flv@mail.com>
2018-04-20 13:55:22 +0300

    AcquisitionService: initial implementation

---
+++ app/src/main/java/dunca/github/io/logpurchasemanager/service/AcquisitionService.java
@@ -0,0 +1,36 @@
+package dunca.github.io.logpurchasemanager.service;
+
+import java.util.List;
+
+import dunca.github.io.logpurchasemanager.service.inferfaces.Service;
+import io.github.dunca.logpurchasemanager.shared.model.custom.FullAcquisition;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public class AcquisitionService extends Service {
+    private AcquisitionServiceImpl mService = RETROFIT.create(AcquisitionServiceImpl.class);
+    private static AcquisitionService sInstance;
+
+    private AcquisitionService() {
+
+    }
+
+    public synchronized static AcquisitionService getInstance() {
+        if (sInstance == null) {
+            sInstance = new AcquisitionService();
+        }
+
+        return sInstance;
+    }
+
+    public void postFullAcquisitionList(Callback<List<FullAcquisition>> callback,
+                                        List<FullAcquisition> fullAcquisitionList) {
+        sendRequest(mService.postFullAcquisitionList(fullAcquisitionList), callback);
+    }
+
+    interface AcquisitionServiceImpl {
+        @POST("fullacquisition")
+        Call<List<FullAcquisition>> postFullAcquisitionList(@Body List<FullAcquisition> fullAcquisitionList);
+    }
+}
