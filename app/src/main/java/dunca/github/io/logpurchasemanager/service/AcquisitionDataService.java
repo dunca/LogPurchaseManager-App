dbd76d9bc6b58c080e4fb30603c1cedf50ad0204
dunca <flv@mail.com>
2018-04-21 13:02:48 +0300

    AcquisitionService: renamed to AcquisitionDataService and removed unnecessary code

---
+++ app/src/main/java/dunca/github/io/logpurchasemanager/service/AcquisitionDataService.java
@@ -0,0 +1,33 @@
+package dunca.github.io.logpurchasemanager.service;
+
+import dunca.github.io.logpurchasemanager.service.inferfaces.Service;
+import io.github.dunca.logpurchasemanager.shared.model.custom.AcquisitionData;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public class AcquisitionDataService extends Service {
+    private AcquisitionDataServiceImpl mService = RETROFIT.create(AcquisitionDataServiceImpl.class);
+    private static AcquisitionDataService sInstance;
+
+    private AcquisitionDataService() {
+
+    }
+
+    public synchronized static AcquisitionDataService getInstance() {
+        if (sInstance == null) {
+            sInstance = new AcquisitionDataService();
+        }
+
+        return sInstance;
+    }
+
+    public void postAcquisitionData(Callback<AcquisitionData> callback, AcquisitionData acquisitionData) {
+        sendRequest(mService.postAcquisitionData(acquisitionData), callback);
+    }
+
+    interface AcquisitionDataServiceImpl {
+        @POST("acquisition_data")
+        Call<AcquisitionData> postAcquisitionData(@Body AcquisitionData acquisitionData);
+    }
+}
